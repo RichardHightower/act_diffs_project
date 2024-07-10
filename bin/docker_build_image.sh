ff4f2469ff7306e99c05512e40788db91ba56964
Rick Hightower <richardhightower@gmail.com>
2024-07-10 12:00:50 -0500

    updated scripts

--- bin/docker_build_image.sh
+++ bin/docker_build_image.sh
@@ -11,16 +11,17 @@ VERSION=$(cat version.txt)
 # Set variables
 IMAGE_NAME="act_diffs_project"
 GITREPO_USERNAME="richardhightower"  # Change this to your GitHub username
+PLATFORM="linux/amd64,linux/arm64"  # Build for multiple platforms
 
 # Build the Docker image
 echo "Building Docker image version $VERSION..."
-docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest .
+docker buildx create --use
+docker buildx build --platform $PLATFORM -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest --push .
 
 # Tag the image for GitHub Container Registry
-docker tag $IMAGE_NAME:$VERSION ghcr.io/$GITREPO_USERNAME/$IMAGE_NAME:$VERSION
-docker tag $IMAGE_NAME:$VERSION ghcr.io/$GITREPO_USERNAME/$IMAGE_NAME:latest
+docker buildx build --platform $PLATFORM -t ghcr.io/$GITREPO_USERNAME/$IMAGE_NAME:$VERSION -t ghcr.io/$GITREPO_USERNAME/$IMAGE_NAME:latest --push .
 
-echo "Docker image built and tagged as:"
+echo "Docker image built and pushed as:"
 echo "ghcr.io/$GITREPO_USERNAME/$IMAGE_NAME:$VERSION"
 echo "ghcr.io/$GITREPO_USERNAME/$IMAGE_NAME:latest"
 
